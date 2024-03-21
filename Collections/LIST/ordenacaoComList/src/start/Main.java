4c73aba2192a14dca96e23b8c6eb1ccf5dab23c0
antonio gabriel <antonio.gabrielTI@outlook.com>
2024-03-21 14:40:29 -0300

    add final ex. ordenacao LIST
    Signed-off-by: antonio gabriel <antonio.gabrielTI@outlook.com>

--- Collections/LIST/ordenacaoComList/src/start/Main.java
+++ Collections/LIST/ordenacaoComList/src/start/Main.java
@@ -1,21 +1,47 @@
 package start;
 
+import utilidade.OrdenacaoPessoa;
+
 import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     static Scanner sc = new Scanner(System.in);
+    static OrdenacaoPessoa pessoa = new OrdenacaoPessoa();
     public static void main(String[] args) {
         int escolha;
-
-        do{
+        do {
             System.out.println("Exercícios de ordenação com List");
             System.out.println("1 - Adicionar pessoa\n" +
                     "2 - Ordenar por idade\n" +
                     "3 - Ordenar por altura\n" +
                     "0 - Sair");
             escolha = sc.nextInt();
-        }while (escolha != 0);
+
+            switch (escolha) {
+                case 1:
+                    adicionarPessoa();
+                    break;
+                case 2:
+                    pessoa.ordenarPorIdade();
+                    break;
+                case 3:
+                    pessoa.ordenarPorAltura();
+                    break;
+            }
+        } while (escolha != 0);
     }
+
+    public static void adicionarPessoa() {
+        sc.nextLine();
+        System.out.println("Digite o nome da pessoa: ");
+        String nome = sc.nextLine();
+        System.out.println("Digite a idade da pessoa: ");
+        int idade = sc.nextInt();
+        System.out.println("Digite a altura da pessoa (formato esperaro 0,00): ");
+        float altura = sc.nextFloat();
+        pessoa.adicionarPessoa(nome, idade, altura);
+    }
+
 }
--- Collections/LIST/ordenacaoComList/src/utilidade/OrdenacaoPessoa.java
+++ Collections/LIST/ordenacaoComList/src/utilidade/OrdenacaoPessoa.java
@@ -8,7 +8,7 @@ import java.util.List;
 public class OrdenacaoPessoa {
     private List<Pessoa> listPessoas;
 
-    public OrdenacaoPessoa(List<Pessoa> listPessoas) {
+    public OrdenacaoPessoa() {
         this.listPessoas = new ArrayList<>();
     }
 
@@ -17,11 +17,15 @@ public class OrdenacaoPessoa {
     }
 
     public void ordenarPorIdade() {
+        System.out.println("Orddem atual: " + listPessoas);
         listPessoas.sort((p1, p2) -> p1.getIdade() - p2.getIdade());
+        System.out.println("Ordem nova: " + listPessoas);
     }
 
     public void ordenarPorAltura() {
+        System.out.println("Orddem atual: " + listPessoas);
         listPessoas.sort((p2, p1) -> Float.compare(p1.getAltura(), p2.getAltura()));
+        System.out.println("Ordem nova: " + listPessoas);
     }
 
 }
