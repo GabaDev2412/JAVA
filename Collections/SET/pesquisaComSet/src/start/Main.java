30d10bd45f3d3adbceefd81a8cef21fd2e8a6ea0
Antonio Gabriel <antonio.gabrielTI@outlook.com>
2024-05-30 09:40:13 -0300

    add update main SET and class contato

--- Collections/SET/pesquisaComSet/src/start/Main.java
+++ Collections/SET/pesquisaComSet/src/start/Main.java
@@ -1,8 +1,28 @@
 package start;
 
+import java.util.Scanner;
+
 public class Main {
+    static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
+        System.out.println("Exercício pesquisa com SET");
+        int escolha;
 
-
+        do{
+            System.out.println("1 - Agenda de Contatos" +
+                    "\n2 - Lista de tarefas" +
+                    "\n0 - Sair");
+            escolha = sc.nextInt();
+            switch (escolha){
+                case 1:
+                    System.out.println("Agenda de Contatos");
+                    //agendaContatos();
+                    break;
+                case 2:
+                    System.out.println("Lista de tarefas");
+                    //listaTarefas();
+                    break;
+            }
+        }while (escolha != 0);
     }
 }
