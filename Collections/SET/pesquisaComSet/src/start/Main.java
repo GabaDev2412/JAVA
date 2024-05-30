9e479cfcd82c89d24bff8175d67b06d028d4dd05
Antonio Gabriel <antonio.gabrielTI@outlook.com>
2024-05-30 16:50:39 -0300

    update menu pesquisa SET file: start.java

--- Collections/SET/pesquisaComSet/src/start/Main.java
+++ Collections/SET/pesquisaComSet/src/start/Main.java
@@ -1,5 +1,7 @@
 package start;
 
+import utilidade.AgendaContatos;
+
 import java.util.Scanner;
 
 public class Main {
@@ -16,13 +18,27 @@ public class Main {
             switch (escolha){
                 case 1:
                     System.out.println("Agenda de Contatos");
-                    //agendaContatos();
+                    agendaContatos();
                     break;
                 case 2:
                     System.out.println("Lista de tarefas");
                     //listaTarefas();
                     break;
             }
+        }while (escolha != 0);
+    }
+
+    private static void agendaContatos(){
+        AgendaContatos agendaContatos = new AgendaContatos();
+        int escolha;
+
+        do{
+            System.out.println("\n1 - Adicionar Contatos" +
+                    "\n2 - Exibir Contatos" +
+                    "\n3 - Pesquisar por Nome" +
+                    "\n4 - Atualizar Numero Contato" +
+                    "\n0 - Sair");
+            escolha = sc.nextInt();
         }while (escolha != 0);
     }
 }
