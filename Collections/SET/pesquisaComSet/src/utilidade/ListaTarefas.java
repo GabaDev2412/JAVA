fae527ff0ba6e140cf8123e602bafc3b58e37b9d
Antonio Gabriel <antonio.gabrielTI@outlook.com>
2024-06-07 12:53:27 -0300

    add update pesquisa SET lista tarefas

--- Collections/SET/pesquisaComSet/src/utilidade/ListaTarefas.java
+++ Collections/SET/pesquisaComSet/src/utilidade/ListaTarefas.java
@@ -8,7 +8,47 @@ import java.util.Set;
 public class ListaTarefas {
     private Set<Tarefa> tarefaSet;
 
-    public ListaTarefas() {this.tarefaSet = new HashSet<>();}
+    public ListaTarefas() {this.tarefaSet = new HashSet<>(); }
+
+    public void adicionarTarefa(String descricao) {
+        tarefaSet.add(new Tarefa(descricao.toLowerCase()));
+    }
+
+    public void removerPorDescricao(String descricao){
+        Tarefa tarefaParaRemover = null;
+        if(!tarefaSet.isEmpty()){
+            for (Tarefa t : tarefaSet) {
+                if (t.getDescricao().startsWith(descricao.toLowerCase())) {
+                    tarefaParaRemover = t;
+                    break;
+                }
+            }
+            tarefaSet.remove(tarefaParaRemover);
+        } else {
+            throw new RuntimeException("Tarefa não encontrada!");
+        }
+    }
+
+    public void exibirTarefas() {
+        if(!tarefaSet.isEmpty()){
+            System.out.println(tarefaSet);
+        }else {
+            throw new RuntimeException("Sem tarefas cadastradas!");
+        }
+    }
+
+    public int contarTarefas(){
+        return tarefaSet.size();
+    }
+
+    public Set<Tarefa> getTarefaConcluida(){
+        Set<Tarefa> tarefasConcluidas = new HashSet<>();
+        for(Tarefa t : tarefaSet){
+            if(t.isCheckTarefa()){
+                tarefasConcluidas.add(t);
+            }
+        }
+    }
 
 }
 
